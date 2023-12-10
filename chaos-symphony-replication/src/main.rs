ca35de75ec09f4ff65e7f667df9ab32cac9da878
agabani <agabani@users.noreply.github.com>
2023-12-10 13:59:36 +0000

    feat: add bevy

--- chaos-symphony-replication/src/main.rs
+++ chaos-symphony-replication/src/main.rs
@@ -3,6 +3,12 @@
 
 //! Chaos Symphony Replication
 
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins(MinimalPlugins);
+
+    app.run();
 }
